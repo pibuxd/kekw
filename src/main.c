7551ab903801212c0ee7583dce6715e14aa679c5
pibuxd <pbublik@v-lo.krakow.pl>
2021-08-20 11:06:59 +0200

    remove early stage of testing in main xD

--- src/main.c
+++ src/main.c
@@ -2,25 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void ast_walking(AST* ast, int side)
-{
-  // if(ast->token->type == TOKEN_INT)
-  // {
-  //   printf("%d: int -> %s\n", side, ast->token->value);
-  // }
-  // else if(ast->token->type == TOKEN_ID)
-  // {
-  //   printf("%d: id -> %s\n", side, ast->token->value);
-  // }
-  printf("%d: node -> %s\n", side, ast->token->value);
-
-  if(ast->right != NULL)
-    ast_walking(ast->right, 1);
-
-  if(ast->left != NULL)
-    ast_walking(ast->left, 0);
-}
-
 int main(int argc, char* argv[])
 {
   Lexer* lexer = NULL;
@@ -40,12 +21,4 @@ int main(int argc, char* argv[])
   parser_compound(parser);
   puts("\nvisiting...\n");
   visit_compound(parser);
-
-  // ast_walking(parser->ast[1], -1);
-  // ast_walking(parser->ast[2], -1);
-
-  // visit_assign_var(parser, parser->ast[1]);
-  // visit_assign_var(parser, parser->ast[2]);
-  // printf("xd: %d", parser->ids['y'-'a'+1]);
-  // ast_walking(parser->ast[2], -1);
 }
