fc147b1ec338be7fc2927f46e187294c1b18f98a
pibuxd <pbublik@v-lo.krakow.pl>
2021-08-22 10:07:22 +0200

    todo new syntax "if"

--- src/main.c
+++ src/main.c
@@ -11,9 +11,13 @@ int main(int argc, char* argv[])
   // char* input = calloc(1000, sizeof(char));
   // fgets(input, 1000, stdin);
   lexer = new_lexer(
-    "var x = (0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1);\n"
-    "var y = x > 5;\n"
+    "var x = (0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1);\n" // -258
+    "var y = x < 5;\n" // 1
     "print((0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1), x, y, \"XD\");\n"
+
+    "if y > x {\n"
+    "print(\"y >>>> x\");\n"
+    "}\n"
   );
   
   token = lexer_get_next_token(lexer);
