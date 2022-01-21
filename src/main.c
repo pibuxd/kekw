ab5887fd2f2f1b4bba729a6eab3b1b6f4140dd7b
pibuxd <bobrasio@gmail.com>
2022-01-21 21:42:57 +0100

    manual testing functions

--- src/main.c
+++ src/main.c
@@ -12,12 +12,16 @@ int main(int argc, char* argv[])
   // fgets(input, 1000, stdin);
   lexer = new_lexer(
     "func xd(a, b){\n"
-    "print(\"jd\");\n"
-    "};\n"
+    "print(\"XDXDXDX\");\n"
     "var x = (0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1);\n" // -258
     "var y = x < 5;\n" // 1
-    "print((0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1), x, y, \"XD\");\n"
+    "print((0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1) + 1000, x, y, \"XD\");\n"
+    "};\n"
+    "func lol(a, b){\n"
+    "print(\"LOLO\");\n"
+    "};\n"
     "xd(5, 7);\n"
+    "lol(2, 4);"
     // "if y > x {\n"
     // "print(\"y >>>> x\");\n"
     // "};\n"
