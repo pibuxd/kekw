91685470a6d45868b048c6a566d0db38fded3ebd
pibuxd <bobrasio@gmail.com>
2022-01-22 15:16:44 +0100

    read from file and beautify code

--- src/main.c
+++ src/main.c
@@ -1,36 +1,60 @@
 #include "include/visitor.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char* read_file(char *filename){
+  FILE *fp = fopen(filename, "rb"); // Open in binary mode
+  int seek = fseek(fp, 0, SEEK_END);
+  if(seek != 0)
+  {
+    printf("file \"%s\" doesn't exists\n", filename);
+    fclose(fp);
+    exit(-1);
+  }
+
+  size_t filesize = (size_t)ftell(fp);
+  char* filedata = calloc(filesize + 1, 1); // Add 1 for the terminating "nul" character
+  rewind(fp);
+  fread(filedata, 1, filesize, fp); // Read whole file
+  fclose(fp);
+
+  return filedata;
+}
 
 int main(int argc, char* argv[])
 {
+  if(argc == 1)
+  {
+    puts("no such file given");
+    exit(-1);
+  }
+  if(argc > 2)
+  {
+    puts("too much arguments given");
+  }
+
+  char *filename = calloc(strlen(argv[1]), sizeof(char));
+  strcpy(filename, argv[1]);
+  char* filedata = read_file(filename);
+
   Lexer* lexer = NULL;
   Token* token = NULL;
   Parser* parser = NULL;
 
-  // char* input = calloc(1000, sizeof(char));
-  // fgets(input, 1000, stdin);
-  lexer = new_lexer(
-    "func xd(a, b){\n"
-    "print(\"XDXDXDX\");\n"
-    "var x = (0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1);\n" // -258
-    "var y = x < 5;\n" // 1
-    "print((0 - 1) * (9 * 5 + 10 - 3)*5 + 1 - 2 - 3 * (0 - 1) + 1000, x, y, \"XD\");\n"
-    "};\n"
-    "func lol(a, b){\n"
-    "print(\"LOLO\");\n"
-    "};\n"
-    "xd(5, 7);\n"
-    "lol(2, 4);"
-    // "if y > x {\n"
-    // "print(\"y >>>> x\");\n"
-    // "};\n"
-  );
-  
+  lexer = new_lexer(filedata);  
   token = lexer_get_next_token(lexer);
   parser = new_parser(lexer, token);
 
   parser_compound(parser);
   puts("\nvisiting...\n");
   visit_compound(parser);
+
+  free(filename);
+  free(filedata);
+  free(lexer);
+  free(token);
+  free(parser);
+  
+  return 0;
 }
